c1b1be9be3b94d86d19c4fe37eeb63756e2abcc0
kmcalpine <kurtismcalpine@gmail.com>
2023-04-28 06:35:22 +0100

    add frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
+import { UserConfig } from "vite";
+
+const config: UserConfig = {
+	plugins: [react(), ssr()],
+	ssr: {
+		noExternal: ["styled-components", "@emotion/*"],
+	},
+};
+
+export default config;
